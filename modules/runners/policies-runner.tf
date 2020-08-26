68eb2b4f70b9f66ada5c9eb0abbfb39cf1341f86
Paul Kubitschek <paul.kubitschek@hudl.com>
2020-08-26 17:05:31 -0500

    Update policies-runner.tf

--- modules/runners/policies-runner.tf
+++ modules/runners/policies-runner.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "runner" {
-  count = var.instance_role == null ? 1 : 0
+  count                = "${var.instance_role}" == null ? 1 : 0
   name                 = "${var.environment}-github-action-runners-runner-role"
   assume_role_policy   = templatefile("${path.module}/policies/instance-role-trust-policy.json", {})
   path                 = local.role_path
@@ -10,7 +10,7 @@ resource "aws_iam_role" "runner" {
 }
 
 data "aws_iam_role" "custom_runner" {
-  count = var.instance_role == null ? 0 : 1
+  count = "${var.instance_role}" == null ? 0 : 1
   name = "${var.instance_role}"
 }
 
@@ -21,14 +21,14 @@ resource "aws_iam_instance_profile" "runner" {
 }
 
 resource "aws_iam_role_policy_attachment" "runner_session_manager_aws_managed" {
-  count      = var.enable_ssm_on_runners ? 1 : 0
-  role = var.instance_role == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
+  count      = "${var.enable_ssm_on_runners}" ? 1 : 0
+  role       = "${var.instance_role}" == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy" "ssm_parameters" {
-  name = "runner-ssm-parameters"
-  role = var.instance_role == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
+  name   = "runner-ssm-parameters"
+  role   = "${var.instance_role}" == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
   policy = templatefile("${path.module}/policies/instance-ssm-parameters-policy.json",
     {
       arn_ssm_parameters = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${var.environment}-*"
@@ -37,8 +37,8 @@ resource "aws_iam_role_policy" "ssm_parameters" {
 }
 
 resource "aws_iam_role_policy" "dist_bucket" {
-  name = "distribution-bucket"
-  role = var.instance_role == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
+  name   = "distribution-bucket"
+  role   = "${var.instance_role}" == null ? aws_iam_role.runner[0].name : data.aws_iam_role.custom_runner[0].name
   policy = templatefile("${path.module}/policies/instance-s3-policy.json",
     {
       s3_arn = var.s3_bucket_runner_binaries.arn

23658d7e65e93e0cd385dfad2914181fee35309d
Jared Barboza <codeimpossible@users.noreply.github.com>
2020-08-26 14:43:44 -0500

    Update policies-runner.tf

--- modules/runners/policies-runner.tf
+++ modules/runners/policies-runner.tf
@@ -16,19 +16,19 @@ data "aws_iam_role" "custom_runner" {
 
 resource "aws_iam_instance_profile" "runner" {
   name = "${var.environment}-github-action-runners-profile"
-  role = var.instance_role == null ? aws_iam_role.runner.name : aws_iam_role.custom_runner.name
+  role = var.instance_role == null ? aws_iam_role.runner[0].name : aws_iam_role.custom_runner[0].name
   path = local.instance_profile_path
 }
 
 resource "aws_iam_role_policy_attachment" "runner_session_manager_aws_managed" {
   count      = var.enable_ssm_on_runners ? 1 : 0
-  role = var.instance_role == null ? aws_iam_role.runner.name : aws_iam_role.custom_runner.name
+  role = var.instance_role == null ? aws_iam_role.runner[0].name : aws_iam_role.custom_runner[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy" "ssm_parameters" {
   name = "runner-ssm-parameters"
-  role = var.instance_role == null ? aws_iam_role.runner.name : aws_iam_role.custom_runner.name
+  role = var.instance_role == null ? aws_iam_role.runner[0].name : aws_iam_role.custom_runner[0].name
   policy = templatefile("${path.module}/policies/instance-ssm-parameters-policy.json",
     {
       arn_ssm_parameters = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${var.environment}-*"
@@ -38,7 +38,7 @@ resource "aws_iam_role_policy" "ssm_parameters" {
 
 resource "aws_iam_role_policy" "dist_bucket" {
   name = "distribution-bucket"
-  role = var.instance_role == null ? aws_iam_role.runner.name : aws_iam_role.custom_runner.name
+  role = var.instance_role == null ? aws_iam_role.runner[0].name : aws_iam_role.custom_runner[0].name
   policy = templatefile("${path.module}/policies/instance-s3-policy.json",
     {
       s3_arn = var.s3_bucket_runner_binaries.arn

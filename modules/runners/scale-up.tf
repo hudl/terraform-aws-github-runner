a4ae81f0c22442b90a342b797ee874d632df12e7
Paul Kubitschek <paul.kubitschek@hudl.com>
2020-08-26 17:35:18 -0500

    Update scale-up.tf

--- modules/runners/scale-up.tf
+++ modules/runners/scale-up.tf
@@ -13,6 +13,7 @@ resource "aws_kms_grant" "scale_up" {
 }
 
 resource "aws_lambda_function" "scale_up" {
+  count                          = "${var.instance_role}" == null ? 1 : 0
   filename                       = local.lambda_zip
   source_code_hash               = filebase64sha256(local.lambda_zip)
   function_name                  = "${var.environment}-scale-up"
@@ -35,8 +36,8 @@ resource "aws_lambda_function" "scale_up" {
       GITHUB_APP_CLIENT_ID        = var.github_app.client_id
       GITHUB_APP_CLIENT_SECRET    = local.github_app_client_secret
       SUBNET_IDS                  = join(",", var.subnet_ids)
-      LAUNCH_TEMPLATE_NAME        = aws_launch_template.runner.name
-      LAUNCH_TEMPLATE_VERSION     = aws_launch_template.runner.latest_version
+      LAUNCH_TEMPLATE_NAME        = aws_launch_template.runner[count.index].name
+      LAUNCH_TEMPLATE_VERSION     = aws_launch_template.runner[count.index].latest_version
     }
   }
 }
@@ -69,11 +70,12 @@ resource "aws_iam_role" "scale_up" {
 }
 
 resource "aws_iam_role_policy" "scale_up" {
+  count = "${var.instance_role}" == null ? 1 : 0
   name = "${var.environment}-lambda-scale-up-policy"
   role = aws_iam_role.scale_up.name
 
   policy = templatefile("${path.module}/policies/lambda-scale-up.json", {
-    arn_runner_instance_role = aws_iam_role.runner[0].arn
+    arn_runner_instance_role = aws_iam_role.runner[count.index].arn
     sqs_arn                  = var.sqs_build_queue.arn
   })
 }
